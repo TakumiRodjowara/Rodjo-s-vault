d4ae73c41a6ef5bd9c033e561b378a6d1d91f0a0
TakumiRodjowara <127847578+TakumiRodjowara@users.noreply.github.com>
2023-04-06 11:47:33 +0200

    Add files via upload
    Beautiful

---
+++ cursed.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#define no !
+#define sussin ?
+#define rn ;
+#define cap false
+#define fr long
+#define tho }
+#define gonna {
+#define be ==
+#define bouta &
+#define finna =
+#define mf *
+#define bussin :
+#define yikes break
+#define vibin --
+#define deadass return
+#define ongod ++
+#define imma main
+
+int main(void) gonna
+    
+tho
